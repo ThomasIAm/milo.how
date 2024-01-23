2187c8b25cf9b746b19d0572d988ccf74e40d881
Thomas van den Nieuwenhoff <10333951+ThomasIAm@users.noreply.github.com>
2024-01-23 19:12:50 +0000

    Moar Milo

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var path = '/images/',
-    imgs = ['kont-lik.jpg'],
+    imgs = [
+        'bank.jpg', 'hang.jpg', 'kont-lik.jpg', 'mand.jpg', 'overlap.jpg', 'paal.jpg', 'rek.jpg', 'schuw.jpg', 'stoel.jpg',
+        'strangle.jpg', 'tand.jpg', 'tong.jpg', 'tv.jpg', 'yo.jpg'
+    ],
     i = Math.floor(Math.random()*imgs.length);
 $('.el').append("<img src='"+path+imgs[i]+"'>").hide().fadeIn(2000);
