d913af9d825148e26f477cab3c9cf7e687e6ef32
Thomas van den Nieuwenhoff <10333951+ThomasIAm@users.noreply.github.com>
2024-01-24 10:00:17 +0000

    Milo did the was?!

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var path = '/images/',
     imgs = [
         'bank.jpg', 'hang.jpg', 'kont-lik.jpg', 'mand.jpg', 'overlap.jpg', 'paal.jpg', 'rek.jpg', 'schuw.jpg', 'stoel.jpg',
-        'strangle.jpg', 'tand.jpg', 'tong.jpg', 'tv.jpg', 'yo.jpg'
+        'strangle.jpg', 'tand.jpg', 'tong.jpg', 'tv.jpg', 'yo.jpg', 'was.jpeg'
     ],
     i = Math.floor(Math.random()*imgs.length);
 $('.el').append("<img src='"+path+imgs[i]+"'>").hide().fadeIn(2000);
